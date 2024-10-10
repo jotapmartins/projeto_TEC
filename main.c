e0206498968d716d4ceda8977cde3cda49192522
jotapmartins <martins_jp19@yahoo.com>
2024-10-10 23:48:43 +0000

    atualizado

--- main.c
+++ main.c
@@ -2,15 +2,35 @@
 #include <string.h>
 
 void menuPrincipal() {
-  printf("\n");
-  printf("Qual o problema do seu celular? \n");
-  printf("\n");
-  printf("1. Vidro frontal quebrado.\n");
-  printf("2. Vidro traseiro quebrado.\n");
-  printf("3. Problema com a bateria.\n");
-  printf("4. Câmera quebrada. \n");
-  printf("5. Placa queimada.\n");
-  printf("6. Outros...\n");
+    printf("\n");
+    printf("Qual o problema do seu celular? \n");
+    printf("\n");
+    printf("1. Vidro frontal quebrado.\n");
+    printf("2. Vidro traseiro quebrado.\n");
+    printf("3. Problema com a bateria.\n");
+    printf("4. Câmera quebrada.\n");
+    printf("5. Placa queimada.\n");
+    printf("6. Outros...\n");
+    printf("7. Sair\n");
+}
+
+void respostaOpcao(int opcao) {
+    switch (opcao) {
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+            printf("\nPor favor, informe qual o modelo do seu aparelho.\n");
+            break;
+        case 7:
+            printf("\nSaindo do programa...\n");
+            break;
+        default:
+            printf("\nOpção inválida! Por favor, escolha uma opção válida do menu.\n");
+            break;
+    }
 }
 
 void cadastrarUsuario() {
@@ -20,7 +40,7 @@ void cadastrarUsuario() {
 
     BD = fopen("usuario.txt", "a");
     if (BD == NULL) {
-        printf("Erro!!!\n");
+        printf("Erro ao abrir o arquivo!!!\n");
         return;
     }
 
@@ -40,10 +60,10 @@ int efetuarLogin() {
     char cpfCadastrado[12];
     char senhaCadastrada[20];
     FILE *BD;
-  
+
     BD = fopen("usuario.txt", "r");
     if (BD == NULL) {
-        printf("Sem usuario!!!\n");
+        printf("Sem usuários cadastrados!!!\n");
         return 0;
     }
 
@@ -61,31 +81,50 @@ int efetuarLogin() {
     }
 
     fclose(BD);
-
     printf("CPF ou senha incorretos!\n");
     return 0;
+}
+
+int executarPrograma() {
+    int opcao;
+
+    do {
+        menuPrincipal();
+        printf("Escolha uma opção: ");
+        scanf("%d", &opcao);
+
+        respostaOpcao(opcao);
+
+        if (opcao == 7) {
+            return 0;
+        }
+
+    } while (1);
 }
 
 int main(void) {
     int opcao;
 
-    printf("Bem vindo à assistencia tecnica! \n");
-    printf("1 - Cadastrar usuário \n");
-    printf("2 - Fazer login\n");
-    printf("Escolha uma opcao para entrar: ");
-    scanf("%d", &opcao);
+    do {
+        printf("\nBem-vindo à assistência técnica!\n");
+        printf("1 - Cadastrar usuário\n");
+        printf("2 - Fazer login\n");
+        printf("Escolha uma opção para entrar: ");
+        scanf("%d", &opcao);
 
-    if (opcao == 1) {
-        cadastrarUsuario();
-    } else if (opcao == 2) {
-        efetuarLogin();
-    } else {
-        printf("Opcao invalida.\n");
-    }
+        if (opcao == 1) {
+            cadastrarUsuario();
+        } else if (opcao == 2) {
+            if (efetuarLogin()) {
+                if (executarPrograma() == 0) {
+                    break;
+                }
+            }
+        } else {
+            printf("Opção inválida.\n");
+        }
 
-    menuPrincipal();
-    printf("Escolha uma opção: ");
-    scanf("%d", &opcao);
+    } while (1);
 
     return 0;
 }
